6888f2b570e38369a24019031f8052cdf04f013c
Karankevich <liza_minyaeva@mail.ru>
2023-07-27 15:20:42 +0300

    fixes

--- catalog/views.py
+++ catalog/views.py
@@ -19,6 +19,10 @@ class ContactView(TemplateView):
         'title': 'Контакты'
     }
 
+    def post(self, request, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)
+
     def get_context_data(self, **kwargs):
         if self.request.method == 'POST':
             name = self.request.POST.get('name')
