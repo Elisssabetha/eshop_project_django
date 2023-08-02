8c7547cad977c4a5a05745003d75eaa3d16965b3
Karankevich <liza_minyaeva@mail.ru>
2023-08-02 22:11:46 +0300

    Формы

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
 from catalog.views import ProductListView, ContactView, ProductDetailView, ProductUpdateView, BlogCreateView, \
-    BlogUpdateView, BlogDetailView, BlogListView, BlogDeleteView
+    BlogUpdateView, BlogDetailView, BlogListView, BlogDeleteView, ProductDeleteView, ProductCreateView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('contacts/', ContactView.as_view(), name='contacts'),
+    path('create_product', ProductCreateView.as_view(), name='product_create'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_view'),
     path('edit/<int:pk>/', ProductUpdateView.as_view(), name='product_edit'),
+    path('delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
     path('create_blog/', BlogCreateView.as_view(), name='create_blog'),
     path('edit_blog/<slug:slug>/', BlogUpdateView.as_view(), name='edit_blog'),
     path('view_blog/<slug:slug>/', BlogDetailView.as_view(), name='view_blog'),
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,11 @@
+from django.forms import inlineformset_factory
 from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, TemplateView, DetailView, UpdateView, CreateView, DeleteView
 from pytils.translit import slugify
 
-from catalog.models import Product, Blog
+from catalog.forms import ProductForm, VersionForm
+from catalog.models import Product, Blog, Version
 
 
 class ProductListView(ListView):
@@ -13,32 +15,60 @@ class ProductListView(ListView):
     }
 
 
-class ContactView(TemplateView):
-    template_name = 'catalog/contacts.html'
-    extra_context = {
-        'title': 'Контакты'
-    }
-
-    def post(self, request, **kwargs):
-        context = self.get_context_data(**kwargs)
-        return self.render_to_response(context)
-
-    def get_context_data(self, **kwargs):
-        if self.request.method == 'POST':
-            name = self.request.POST.get('name')
-            email = self.request.POST.get('email')
-            message = self.request.POST.get('message')
-            print(f'You have new message from {name} ({email}): {message}')
-        return super().get_context_data(**kwargs)
-
-
 class ProductDetailView(DetailView):
     model = Product
 
 
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('product_list')
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
+        if self.request.method == 'POST':
+            context_data['formset'] = VersionFormset(self.request.POST)
+        else:
+            context_data['formset'] = VersionFormset()
+        return context_data
+
+    def form_valid(self, form):
+        formset = self.get_context_data()['formset']
+        self.object = form.save()
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+        return super().form_valid(form)
+
+
 class ProductUpdateView(UpdateView):
     model = Product
-    fields = ('name', 'description', 'preview', 'price')
+    form_class = ProductForm
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
+        if self.request.method == 'POST':
+            context_data['formset'] = VersionFormset(self.request.POST, instance=self.object)
+        else:
+            context_data['formset'] = VersionFormset(instance=self.object)
+        return context_data
+
+    def form_valid(self, form):
+        formset = self.get_context_data()['formset']
+        self.object = form.save()
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('product_view', args=[self.kwargs.get('pk')])
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
     success_url = reverse_lazy('product_list')
 
 
@@ -100,6 +130,25 @@ class BlogDetailView(DetailView):
 class BlogDeleteView(DeleteView):
     model = Blog
     success_url = reverse_lazy('list_blog')
+
+
+class ContactView(TemplateView):
+    template_name = 'catalog/contacts.html'
+    extra_context = {
+        'title': 'Контакты'
+    }
+
+    def post(self, request, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)
+
+    def get_context_data(self, **kwargs):
+        if self.request.method == 'POST':
+            name = self.request.POST.get('name')
+            email = self.request.POST.get('email')
+            message = self.request.POST.get('message')
+            print(f'You have new message from {name} ({email}): {message}')
+        return super().get_context_data(**kwargs)
 
 # def contacts(request):
 #     context = {
